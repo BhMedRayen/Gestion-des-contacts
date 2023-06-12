bbcc3beb0136fee2c2b20338b022dc4bfdc18fbf
rayen <rayenmohamed2310@gmail.com>
2023-06-12 16:00:59 +0100

    first commit

---
+++ contact.js
@@ -0,0 +1,118 @@
+afficher();
+let NVContact = [];
+let id=0;
+document.getElementById('ContactForm').addEventListener("submit", function(event) {
+  event.preventDefault();
+  let nom = document.getElementsByName('nom')[0].value;
+  let prenom = document.getElementsByName('prenom')[0].value;
+  let mail = document.getElementsByName('mail')[0].value;
+  let cin = document.getElementsByName('cin')[0].value;
+  let genre = document.getElementsByName('genre');
+  let g = "";
+  for (let i = 0; i < genre.length; i++) {
+    if (genre[i].checked) {
+      g = genre[i].value;
+    }
+  }
+  let ContactStockee = localStorage.getItem('contact');
+  let TableauContactStocke = JSON.parse(ContactStockee) || [];
+  while(existe(id))
+  {
+    id+=1;
+  }
+  let NVContact = {
+        ID :id,
+        nom: nom,
+        prenom: prenom,
+        mail: mail,
+        cin: cin,
+        genre: g,
+      };
+  
+ 
+  
+  
+ 
+  TableauContactStocke.push(NVContact);
+  let CNCT = JSON.stringify(TableauContactStocke);
+  localStorage.setItem("contact", CNCT);
+  afficher();
+  let alert=document.getElementById('alert');
+  alert.classList.remove("alerte");
+  alert.innerHTML=`<div class="alert alert-success" role="alert">
+  Ajout avec succes
+</div>`;
+resetForm();
+});
+function resetForm()
+{
+    document.getElementById('ContactForm').reset();
+}
+function afficher ()
+{
+    if(typeof localStorage != undefined)
+    {
+        let ContactString = localStorage.getItem('contact');
+        let TBContact = JSON.parse(ContactString);
+        let card=document.getElementById("card");
+        card.innerHTML = '';
+        if(TBContact && TBContact.length>0)
+        {
+            for(let contact of TBContact)
+             {
+                  card.innerHTML+=`
+                                    <div class="col">
+                                        <div class="card mb-4" style="width: 18rem;">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Contact</h5>
+                                                <h6>Nom</h6>
+                                                <p>${contact.nom}</p>
+                                                <h6>Prenom</h6>
+                                                <p>${contact.prenom}</p>
+                                                <h6>Email</h6>
+                                                <p>${contact.mail}</p>
+                                                <h6>NumCin</h6>
+                                                <p>${contact.cin}</p>
+                                                <h6>Genre</h6>
+                                                <p>${contact.genre}</p>
+                                                <button type="submit" class="btn btn-warning m-2">Modifier</button>
+                                                <button type="reset" class="btn btn-danger m-2" onclick="deleteContact(${contact.ID})">Supprimer</button>
+                                            </div>
+                                            </div>`
+             }
+        }
+        
+    }
+}
+function deleteContact(ContactId)
+{
+   
+    let ContactString=localStorage.getItem('contact');
+    let TBContact=JSON.parse(ContactString);
+    let i=-1;
+    for(let contact of TBContact)
+    {
+        i+=1;
+        let id=parseInt(contact.ID);
+        if(ContactId==id)
+         {
+            TBContact.splice(i,1)
+            let con=JSON.stringify(TBContact);
+            localStorage.setItem('contact',con);
+         }
+    }
+    afficher();
+}
+function existe(id) {
+    let ContactString = localStorage.getItem('contact');
+    let TBContact = JSON.parse(ContactString);
+    if (Array.isArray(TBContact)) {
+      for (let contact of TBContact) {
+        if (contact.ID == id) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
